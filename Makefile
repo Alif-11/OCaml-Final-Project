c3929b66d373af23521be4d55e06605026a82de2
ax38 <ax38@cornell.edu>
2023-10-20 20:30:59 -0400

    Ui progress :)
    -timer now has hundredths of seconds :)
    -you can also type and it will show up
    -the font size is still fucked
    -somebody please ask on ed disucssion
    -i will lose my mind :))))))))))))

--- Makefile
+++ Makefile
@@ -1,7 +1,6 @@
 code:
 	-dune build
 	code .
-	| dune build --watch
 utop: 
 	OCAMLRUNPARAM=b dune exec test/main.exec
 game:
--- _build/default/bin/main.ml
+++ _build/default/bin/main.ml
@@ -1,23 +1,42 @@
-let round start_time = 
+let round_tick start_time cursorX cursorY= 
   let cur_time = Unix.gettimeofday () in
   let time_passed = 
       int_of_float (Float.round ((cur_time -. start_time) *. 100.0)) in
+  let frac_sec = time_passed mod 100 in
+  let string_frac_sec = if frac_sec < 10 then "0" ^ string_of_int frac_sec
+    else string_of_int frac_sec in
   let sec = time_passed / 100 mod 60 in
   let string_sec = if sec >= 10 then string_of_int sec 
     else "0" ^ string_of_int sec in
   let min = time_passed / 6000 in
-  let time_string = string_of_int min ^ ":" ^ string_sec in
+  let time_string = string_of_int min ^ ":" ^ string_sec ^ "." ^ string_frac_sec in
   print_endline time_string;
-  Graphics.moveto 0 0;
+  Graphics.moveto 0 1375;
   Graphics.set_color Graphics.white;
-  Graphics.fill_rect 0 0 300 300;
+  Graphics.fill_rect 0 1375 100 30;
   Graphics.set_color Graphics.black;
-  Graphics.draw_string time_string
+  Graphics.draw_string time_string;
+  match Graphics.key_pressed () with
+  |true ->  
+    Graphics.moveto cursorX cursorY;
+    print_endline (string_of_int cursorX ^ " " ^ string_of_int cursorY);
+    Graphics.draw_char (Graphics.read_key ());
+    Graphics.current_point ()
+  |false -> (cursorX, cursorY)
+
+let round ()= 
+  let start_time = Unix.gettimeofday () in
+  let cursorX = ref 0 in
+  let cursorY = ref 1000 in
+  while true do
+    Unix.sleepf 0.001;
+    match cursorX with | {contents = integer} -> let xpos = integer in 
+    match cursorY with | {contents = integer} -> let ypos = integer in
+    let bruh = round_tick start_time xpos ypos in
+    cursorX := fst bruh;
+    cursorY := snd bruh;
+  done
 
 let () = 
-    Graphics.open_graph " 3000x2000+0+0";
-    let start_time = Unix.gettimeofday () in
-  while true do
-    Unix.sleep 1;
-    round start_time;
-  done
+  Graphics.open_graph " 1500x1500+0+0";
+  round ();
