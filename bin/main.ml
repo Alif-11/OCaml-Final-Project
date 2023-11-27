d7b066c106752db2c5385fc7c576f332a2be1f8e
Liesel-W <lyw8@cornell.edu>
2023-11-27 13:03:19 -0500

    update main.ml with a mutable normal game state

--- bin/main.ml
+++ bin/main.ml
@@ -1,60 +1,72 @@
 open TypeGame
 
-type round_state = 
-  {pos : int * int; str : string; score : int; fin : bool}
+type round_state = {
+  pos : int * int;
+  str : string;
+  score : int;
+  fin : bool;
+}
 
 let new_line = function
-|(x, y) -> if x < 800 then ()
-  else Graphics.moveto 100 (y - 25)
+  | x, y -> if x < 800 then () else Graphics.moveto 100 (y - 25)
 
-let round_tick start_time time_allotted (state : round_state) = 
+let round_tick start_time time_allotted (state : round_state) =
   let cur_time = Unix.gettimeofday () in
-  let time_passed = 
-      int_of_float (Float.round ((cur_time -. start_time) *. 100.0)) in
+  let time_passed =
+    int_of_float (Float.round ((cur_time -. start_time) *. 100.0))
+  in
   let frac_sec = time_passed mod 100 in
-  let string_frac_sec = if frac_sec < 10 then "0" ^ string_of_int frac_sec
-    else string_of_int frac_sec in
+  let string_frac_sec =
+    if frac_sec < 10 then "0" ^ string_of_int frac_sec
+    else string_of_int frac_sec
+  in
   let sec = time_passed / 100 mod 60 in
-  let string_sec = if sec >= 10 then string_of_int sec 
-    else "0" ^ string_of_int sec in
+  let string_sec =
+    if sec >= 10 then string_of_int sec else "0" ^ string_of_int sec
+  in
   let min = time_passed / 6000 in
-  let time_string = 
-    string_of_int min ^ ":" ^ string_sec ^ "." ^ string_frac_sec in
+  let time_string =
+    string_of_int min ^ ":" ^ string_sec ^ "." ^ string_frac_sec
+  in
   Graphics.moveto 100 900;
   Graphics.set_color Graphics.white;
   Graphics.fill_rect 100 900 100 30;
   Graphics.set_color Graphics.black;
   Graphics.draw_string time_string;
-  if time_passed >= time_allotted then
-    {state with fin = true}
+  if time_passed >= time_allotted then { state with fin = true }
   else
     let x_pos = fst state.pos in
     let y_pos = snd state.pos in
     let curStr = state.str in
     match Graphics.key_pressed () with
-    |true ->  
-      Graphics.moveto x_pos y_pos;
-      let key = Graphics.read_key () in
-      Graphics.draw_char key;
-      if key = ' ' then new_line (Graphics.current_point ());
-      if key = Char.chr 13 then {state with fin = true}
-      else 
-        {state with pos = Graphics.current_point (); 
-        str = curStr ^ String.make 1 key} 
-    |false -> state
+    | true ->
+        Graphics.moveto x_pos y_pos;
+        let key = Graphics.read_key () in
+        Graphics.draw_char key;
+        if key = ' ' then new_line (Graphics.current_point ());
+        if key = Char.chr 13 then { state with fin = true }
+        else
+          {
+            state with
+            pos = Graphics.current_point ();
+            str = curStr ^ String.make 1 key;
+          }
+    | false -> state
 
-  
-
-let round gs = 
+let round gs =
   Graphics.moveto 100 800;
-  let word_list = Game.generate_sequence 
-    (Game.word_bag_t (State.NormalGame.difficulty gs))
-    (State.NormalGame.num_words gs) in
+  let word_list =
+    Game.generate_sequence
+      (Game.word_bag_t (State.NormalGameMutable.difficulty gs))
+      (State.NormalGameMutable.num_words gs)
+  in
   let rec print_words = function
-  |[] -> ();
-  |h :: t -> Graphics.draw_string (h ^ " ");
-    new_line (Graphics.current_point ());
-    print_words t in
+    | [] -> ()
+    | h :: t ->
+        Graphics.draw_string (h ^ " ");
+        new_line (Graphics.current_point ());
+        print_words t
+  in
   print_words word_list;
   let start_time = Unix.gettimeofday () in
   let cursorX = ref 100 in
@@ -64,7 +76,10 @@ let round gs =
   let fin = ref false in
   while fin = ref false do
     Unix.sleepf 0.001;
-    let graphics_state = round_tick start_time 6000 {pos = (!cursorX, !cursorY); str = !str; score = !score; fin = !fin} in
+    let graphics_state =
+      round_tick start_time 6000
+        { pos = (!cursorX, !cursorY); str = !str; score = !score; fin = !fin }
+    in
     cursorX := fst graphics_state.pos;
     cursorY := snd graphics_state.pos;
     str := graphics_state.str;
@@ -72,37 +87,41 @@ let round gs =
     fin := graphics_state.fin
   done
 
-
-let () = 
+let () =
   Graphics.open_graph " 1000x1000+0+0";
   Graphics.moveto 100 800;
   Graphics.set_color Graphics.black;
   Graphics.draw_string "Rules : ";
-  Graphics.moveto 100 775; 
+  Graphics.moveto 100 775;
   Graphics.draw_string "1. You lose health for getting words wrong";
-  Graphics.draw_string "2. Once you press space, we consider that a new word! You can't go back to fix your mistakes :)";
+  Graphics.draw_string
+    "2. Once you press space, we consider that a new word! You can't go back \
+     to fix your mistakes :)";
   Graphics.moveto 100 750;
-  Graphics.draw_string "3. If you don't finish, then any words left over will also cost you some health :)";
+  Graphics.draw_string
+    "3. If you don't finish, then any words left over will also cost you some \
+     health :)";
   Graphics.moveto 100 725;
-  Graphics.draw_string "4. If you have time left over, you can regain some health";
+  Graphics.draw_string
+    "4. If you have time left over, you can regain some health";
   Graphics.moveto 100 700;
   Graphics.draw_string "<PRESS ANY KEY TO CONTINUE>";
-  ignore (Graphics.wait_next_event [Key_pressed]);
+  ignore (Graphics.wait_next_event [ Key_pressed ]);
   Graphics.set_color Graphics.white;
   Graphics.fill_rect 0 0 1000 1000;
-  let gs = State.NormalGame.initialize in
+  let gs = State.NormalGameMutable.initialize () in
   while true do
     Graphics.moveto 100 800;
     Graphics.set_color Graphics.black;
     Graphics.draw_string "Are you ready? <PRESS ANY KEY TO START ROUND>";
-    ignore (Graphics.wait_next_event [Key_pressed]);
+    ignore (Graphics.wait_next_event [ Key_pressed ]);
     Graphics.set_color Graphics.white;
     Graphics.fill_rect 0 0 1000 1000;
     Graphics.set_color Graphics.black;
     round gs;
     Graphics.moveto 100 100;
     Graphics.draw_string "<PRESS ANY KEY TO CONTINUE>";
-    ignore (Graphics.wait_next_event [Key_pressed]);
+    ignore (Graphics.wait_next_event [ Key_pressed ]);
     Graphics.set_color Graphics.white;
-    Graphics.fill_rect 0 0 1000 1000;
+    Graphics.fill_rect 0 0 1000 1000
   done
--- src/state.ml
+++ src/state.ml
@@ -1,3 +1,43 @@
+(* (** The signature of Game State*) module type GameState = sig type t (**
+   Representation type of Game State *)
+
+   val initialize : t (** Create a starting state *)
+
+   val time : t -> int (** Time allotted for round in seconds *)
+
+   val num_words : t -> int (** Number of words to be generated *)
+
+   val difficulty : t -> int (** Difficulty level of words generated *)
+
+   val health : t -> int (** Current health *)
+
+   val max_health : t -> int (** Current max health *)
+
+   val cur_level : t -> int (** Number of rounds player has passed so far *)
+
+   val health_lost : int -> int -> int -> t -> t (** Calculates health lost
+   given time remaining, accuracy, words remaining *)
+
+   val adjust_level : t -> t (** Adjust level depending on number of rounds and
+   *) end
+
+   module NormalGame : GameState = struct type t = { time : int; num_words :
+   int; diff : int; hp : int; max_hp : int; cur_lvl : int; }
+
+   let initialize = { time = 120; num_words = 50; diff = 1; hp = 100; max_hp =
+   100; cur_lvl = 0; }
+
+   let time gs = gs.time let num_words gs = gs.num_words let difficulty gs =
+   gs.diff let health gs = gs.hp let max_health gs = gs.hp let cur_level gs =
+   gs.hp
+
+   let health_lost time_left missed words_left gs = let damage_taken = time_left
+   - missed - words_left in { gs with hp = min (gs.hp + damage_taken) gs.max_hp
+   }
+
+   let adjust_level gs = { gs with cur_lvl = gs.cur_lvl + 1; num_words =
+   gs.num_words + 10 } end *)
+
 (** The signature of a mutable version of Game State*)
 module type GameStateMutable = sig
   val initialize :
@@ -69,43 +109,3 @@ module NormalGameMutable : GameStateMutable = struct
     _cur_level := !_cur_level + 1;
     _num_words := !_num_words + 10
 end
-
-(* (** The signature of Game State*) module type GameState = sig type t (**
-   Representation type of Game State *)
-
-   val initialize : t (** Create a starting state *)
-
-   val time : t -> int (** Time allotted for round in seconds *)
-
-   val num_words : t -> int (** Number of words to be generated *)
-
-   val difficulty : t -> int (** Difficulty level of words generated *)
-
-   val health : t -> int (** Current health *)
-
-   val max_health : t -> int (** Current max health *)
-
-   val cur_level : t -> int (** Number of rounds player has passed so far *)
-
-   val health_lost : int -> int -> int -> t -> t (** Calculates health lost
-   given time remaining, accuracy, words remaining *)
-
-   val adjust_level : t -> t (** Adjust level depending on number of rounds and
-   *) end
-
-   module NormalGame : GameState = struct type t = { time : int; num_words :
-   int; diff : int; hp : int; max_hp : int; cur_lvl : int; }
-
-   let initialize = { time = 120; num_words = 50; diff = 1; hp = 100; max_hp =
-   100; cur_lvl = 0; }
-
-   let time gs = gs.time let num_words gs = gs.num_words let difficulty gs =
-   gs.diff let health gs = gs.hp let max_health gs = gs.hp let cur_level gs =
-   gs.hp
-
-   let health_lost time_left missed words_left gs = let damage_taken = time_left
-   - missed - words_left in { gs with hp = min (gs.hp + damage_taken) gs.max_hp
-   }
-
-   let adjust_level gs = { gs with cur_lvl = gs.cur_lvl + 1; num_words =
-   gs.num_words + 10 } end *)
