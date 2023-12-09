fd886b53a05bcd8ee631ea192057a155d9def4ba
Amy Mai <am2556@cornell.edu>
2023-12-08 21:37:20 -0500

    Add docs
    - added documentation for state.mli

--- src/state.mli
+++ src/state.mli
@@ -1,4 +1,6 @@
+(** A model for mutable game states. *)
 module type GameStateMutable = sig
+
   val initialize :
     (* int list (* if we want to dynamically initialize the game state *) -> *)
     unit ->
@@ -6,55 +8,83 @@ module type GameStateMutable = sig
   (** Set starting state values *)
 
   val time : unit -> int
-  (** Time allotted for round in seconds *)
+  (** Time allotted for round in seconds. *)
 
   val _time : int ref
+  (** Time allotted for round in seconds. *)
+
   val _num_words : int ref
+  (** Number of words to be generated. *)
+
   val _difficulty : int ref
+  (** Difficulty level of words generated. *)
+
   val _health : int ref
+  (** Current health. *)
+
   val _max_health : int ref
+  (** Current max health. *)
+
   val _cur_level : int ref
+  (** Number of rounds player has passed so far. *)
+
   val _score : int ref
+  (** Current player score.*)
+
   val _num_items : int ref
+  (** Number of items generated each time.*)
 
   val num_words : unit -> int
-  (** Number of words to be generated *)
+  (** Gets the number of words to be generated. *)
 
   val difficulty : unit -> int
-  (** Difficulty level of words generated *)
+  (** Gets the difficulty level of words generated. *)
 
   val health : unit -> int
-  (** Current health *)
+  (** Gets the current health. *)
 
   val max_health : unit -> int
-  (** Current max health *)
+  (** Gets the current max health. *)
 
   val cur_level : unit -> int
-  (** Number of rounds player has passed so far *)
+  (** Gets the number of rounds player has passed so far. *)
 
   val score : unit -> int
-  (** Current player score*)
+  (** Gets the current player score.*)
 
   val num_items : unit -> int
-  (** Number of items generated each time*)
+  (** Gets the number of items generated each time.*)
 
   val health_lost : int -> int -> int -> int * int
   (** Calculates health lost given time remaining, words wrong and words
-      remaining *)
+      remaining. *)
 
   val adjust_level : unit -> unit
-  (** Adjust level depending on number of rounds *)
+  (** Adjust level depending on number of rounds. *)
 
   val decrement_level : unit -> unit
-  (** Decrease level *)
+  (** Decrease level.*)
 
   val add_score : int -> int
-  (** add score based on number of correct words as passed in*)
+  (** Add score based on number of correct words as passed in.*)
 end
 
 module NormalGameMutable : GameStateMutable
+(** A module for normal mode. *)
+
 module EasyGameMutable : GameStateMutable
+(** A module for easy mode. *)
+
 module HardGameMutable : GameStateMutable
+(** A module for hard mode. *)
+
 module ExtremeGameMutable : GameStateMutable
+(** A module for extreme mode. *)
+
 module SuddenDeathMutable : GameStateMutable
+(** A module for sudden death mode. *)
+
 module ChaosGameMutable : GameStateMutable
+(** A module for chaos mode. *)
+
+
